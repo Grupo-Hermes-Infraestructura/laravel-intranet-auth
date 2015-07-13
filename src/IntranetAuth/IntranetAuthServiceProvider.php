28b22a8ffe19ad938e06afc72b58e2a7ae7d6d5f
Uziel Bueno <ux.webs@gmail.com>
2015-07-13 13:40:57 -0500

    Commit inicial

---
+++ src/IntranetAuth/IntranetAuthServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Ghi\IntranetAuth;
+
+use Illuminate\Contracts\Config\Repository;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\ServiceProvider;
+
+class IntranetAuthServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @param Repository $config
+     */
+    public function boot(Repository $config)
+    {
+        $model = $config->get('auth.model');
+
+        Auth::extend('intranet', function($app) use($model) {
+            return new IntranetUserAuthProvider($model);
+        });
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ src/IntranetAuth/User.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Ghi\IntranetAuth;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+
+class User extends Model implements AuthenticatableContract, CanResetPasswordContract
+{
+    use AuthenticatableIntranetUser, CanResetPassword;
+
+    /**
+     * @var string
+     */
+    protected $connection = 'igh';
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'usuario';
+
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'idusuario';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['nombre', 'correo', 'clave'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = ['clave', 'remember_token'];
+}
