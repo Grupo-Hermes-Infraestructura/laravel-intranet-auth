353de9f96ae894f78e031a910e9d2989e04e534e
Uziel Bueno <ux.webs@gmail.com>
2015-07-24 01:10:48 -0500

    Se agrega alias para User del core-domain

--- src/IntranetAuth/User.php
+++ src/IntranetAuth/User.php
@@ -2,12 +2,12 @@
 
 namespace Ghi\IntranetAuth;
 
-use Ghi\Core\Models\User;
+use Ghi\Core\Models\User as BaseUser;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends User implements AuthenticatableContract, CanResetPasswordContract
+class User extends BaseUser implements AuthenticatableContract, CanResetPasswordContract
 {
     use AuthenticatableIntranetUser, CanResetPassword;
 }
