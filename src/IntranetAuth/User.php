d5decb9dd6c8b469c5e5165f68c9d282f242eff7
Uziel Bueno <ux.webs@gmail.com>
2015-07-24 00:47:08 -0500

    Se agrega dependencia de ghi/core-domain

--- src/IntranetAuth/User.php
+++ src/IntranetAuth/User.php
@@ -2,45 +2,12 @@
 
 namespace Ghi\IntranetAuth;
 
-use Illuminate\Database\Eloquent\Model;
+use Ghi\Core\Models\User;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Model implements AuthenticatableContract, CanResetPasswordContract
+class User extends User implements AuthenticatableContract, CanResetPasswordContract
 {
     use AuthenticatableIntranetUser, CanResetPassword;
-
-    /**
-     * The database table used by the model.
-     *
-     * @var string
-     */
-    protected $table = 'usuario';
-
-    /**
-     * @var string
-     */
-    protected $primaryKey = 'idusuario';
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = ['nombre', 'correo', 'clave'];
-
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = ['clave', 'remember_token'];
-
-    /**
-     * Consider updated_at and created_at timestamps.
-     *
-     * @var bool
-     */
-    public $timestamps = false;
 }
